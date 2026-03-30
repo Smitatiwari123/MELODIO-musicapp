761edca7320d9cb16c63d28a28d495e6767083f8
Smita Tiwari <tiwarismita222@gmail.com>
2026-03-31 00:59:45 +0530

    Fixed project files

---
+++ tracks.js
@@ -0,0 +1,36 @@
+const TRACKS = [
+{
+title: "Kesariya",
+artist: "Arijit Singh",
+src: "songs/Kesariya.mp3",
+cover: "https://i.pinimg.com/originals/58/e7/e0/58e7e0946841312a9e6bde4159569e99.webp"
+},
+
+{
+title: "Heeriye",
+artist: "Jasleen Royal ft. Arijit Singh",
+src: "songs/Heeriye.mp3",
+cover: "https://c.saavncdn.com/506/Heeriye-Hindi-2023-20230622191002-500x500.jpg"
+},
+
+{
+title: "Apna Bana Le",
+artist: "Arijit Singh",
+src: "songs/ApnaBanaLe.mp3",
+cover: "https://c.saavncdn.com/808/Bhediya-Hindi-2022-20221122155159-500x500.jpg"
+},
+
+{
+title: "Tum Kya Mile",
+artist: "Arijit Singh & Shreya Ghoshal",
+src: "songs/TumKyaMile.mp3",
+cover: "https://c.saavncdn.com/615/Rocky-Aur-Rani-Kii-Prem-Kahaani-Hindi-2023-20230724121003-500x500.jpg"
+},
+
+{
+title: "Chaleya",
+artist: "Arijit Singh",
+src: "songs/Chaleya.mp3",
+cover: "https://c.saavncdn.com/482/Jawan-Hindi-2023-20230914181109-500x500.jpg"
+}
+];
