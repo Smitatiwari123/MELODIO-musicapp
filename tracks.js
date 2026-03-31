8a75e7e0330264182315d0bc05dd590eb7881da9
Smita Tiwari <tiwarismita222@gmail.com>
2026-03-31 17:42:50 +0530

    full  updated

--- tracks.js
+++ tracks.js
@@ -1,98 +1,19 @@
+// tracks.js
 const TRACKS = [
-  {
-    title: "Apna Bana Le",
-    artist: "Arijit Singh",
-    src: "https://res.cloudinary.com/doygacnfz/video/upload/v1774949791/ApnaBanaLe_kwjfjf.mp3",
-    cover: "https://i.ytimg.com/vi/11AQjgYybZk/maxresdefault.jpg"
-  },
-  {
-    title: "Ban Ja Rani",
-    artist: "Guru Randhawa",
-    src: "https://res.cloudinary.com/doygacnfz/video/upload/v1774949722/BanJaRani_paigh0.mp3",
-    cover: "https://i.ytimg.com/vi/hlvbDjksdCg/maxresdefault.jpg"
-  },
-  {
-    title: "Chal Diye Tum Kahan",
-    artist: "Unknown",
-    src: "https://res.cloudinary.com/doygacnfz/video/upload/v1774949756/ChalDiyeTumKahan_ppfeow.mp3",
-    cover: "https://i.ytimg.com/vi/Qhl0bocazGQ/maxresdefault.jpg"
-  },
-  {
-    title: "Chaleya",
-    artist: "Arijit Singh",
-    src: "https://res.cloudinary.com/doygacnfz/video/upload/v1774949720/Chaleya_ltvxdt.mp3",
-    cover: "https://www.redchillies.com/wp-content/uploads/2023/11/News-6-Chaleya-Banner.jpg"
-  },
-  {
-    title: "Heeriye",
-    artist: "Jasleen Royal",
-    src: "https://res.cloudinary.com/doygacnfz/video/upload/v1774949962/Heeriye_z7vp1x.mp3",
-    cover: "https://i.ytimg.com/vi/RLzC55ai0eo/maxresdefault.jpg"
-  },
-  {
-    title: "I Think They Call This Love",
-    artist: "Unknown",
-    src: "https://res.cloudinary.com/doygacnfz/video/upload/v1774949751/IThinkTheyCallThisLove_pd6ow1.mp3",
-    cover: "https://axomlyrics.com/wp-content/uploads/2025/09/i-think-they-call-this-love-lyrics.webp"
-  },
-  {
-    title: "Kalyani",
-    artist: "Unknown",
-    src: "https://res.cloudinary.com/doygacnfz/video/upload/v1774950040/KALYANI_iglkfw.mp3",
-    cover: "https://i.ytimg.com/vi/CqDByohxbtA/sddefault.jpg"
-  },
-  {
-    title: "Kesariya",
-    artist: "Arijit Singh",
-    src: "https://res.cloudinary.com/doygacnfz/video/upload/v1774950094/Kesariya_cxholq.mp3",
-    cover: "https://www.easterneye.biz/wp-content/uploads/2024/05/kesariya-only-indian-song-to-surpass-500-million-streams-on-spotify.jpg"
-  },
-  {
-    title: "Lutt Putt Gaya",
-    artist: "Arijit Singh",
-    src: "https://res.cloudinary.com/doygacnfz/video/upload/v1774950091/LuttPuttGaya_x61tni.mp3",
-    cover: "https://i.ytimg.com/vi/7CdpHATpXXU/maxresdefault.jpg"
-  },
-  {
-    title: "Pink Lips",
-    artist: "Unknown",
-    src: "https://res.cloudinary.com/doygacnfz/video/upload/v1774950181/PinkLips_momoql.mp3",
-    cover: "https://i.ytimg.com/vi/w5VFOKKAbQQ/maxresdefault.jpg"
-  },
-  {
-    title: "Satranga",
-    artist: "Arijit Singh",
-    src: "https://res.cloudinary.com/doygacnfz/video/upload/v1774950194/Satranga_msnsp4.mp3",
-    cover: "https://c.ndtvimg.com/2023-10/s8cqfkd8_-rashmika-_625x300_27_October_23.jpg"
-  },
-  {
-    title: "Tere Liye",
-    artist: "Unknown",
-    src: "https://res.cloudinary.com/doygacnfz/video/upload/v1774950217/TereLiye_ac1xxn.mp3",
-    cover: "https://i.ytimg.com/vi/nOZ3M1O0Wbg/maxresdefault.jpg"
-  },
-  {
-    title: "Tu Hi Mera",
-    artist: "Unknown",
-    src: "https://res.cloudinary.com/doygacnfz/video/upload/v1774950204/TuHiMera_ovbhyy.mp3",
-    cover: "https://i.ytimg.com/vi/yBa3FVQKAvY/maxresdefault.jpg"
-  },
-  {
-    title: "Tufan",
-    artist: "Unknown",
-    src: "https://res.cloudinary.com/doygacnfz/video/upload/v1774950198/Tufan_zh9dim.mp3",
-    cover: "https://img.freepik.com/premium-photo/cute-piggy-simple-flat-clipart-cartoon-illustration-with-white-background-no-shadow-detail_1091925-12850.jpg?w=1380"
-  },
-  {
-    title: "Tera Deedar Hua",
-    artist: "Unknown",
-    src: "https://res.cloudinary.com/doygacnfz/video/upload/v1774950184/TeraDeedarHua_winzxc.mp3",
-    cover: "https://i.ytimg.com/vi/abcd1234/maxresdefault.jpg" // Replace with actual cover if available
-  },
-  {
-    title: "Kabhi Jo Badal Barse",
-    artist: "Arijit Singh",
-    src: "https://res.cloudinary.com/doygacnfz/video/upload/v1774949923/KabhiJoBadalBarse_fuytch.mp3",
-    cover: "https://i.ytimg.com/vi/xyz123/maxresdefault.jpg" // Replace with actual cover if available
-  }
+  { title: "Apna Bana Le", artist: "Arijit Singh", src: "https://res.cloudinary.com/doygacnfz/video/upload/v1774949791/ApnaBanaLe_kwjfjf.mp3", cover: "https://i.ytimg.com/vi/11AQjgYybZk/maxresdefault.jpg" },
+  { title: "Ban Ja Rani", artist: "Guru Randhawa", src: "https://res.cloudinary.com/doygacnfz/video/upload/v1774949722/BanJaRani_paigh0.mp3", cover: "https://i.ytimg.com/vi/hlvbDjksdCg/maxresdefault.jpg" },
+  { title: "Chal Diye Tum Kahan", artist: "Unknown", src: "https://res.cloudinary.com/doygacnfz/video/upload/v1774949756/ChalDiyeTumKahan_ppfeow.mp3", cover: "https://i.ytimg.com/vi/Qhl0bocazGQ/maxresdefault.jpg" },
+  { title: "Chaleya", artist: "Arijit Singh", src: "https://res.cloudinary.com/doygacnfz/video/upload/v1774949720/Chaleya_ltvxdt.mp3", cover: "https://www.redchillies.com/wp-content/uploads/2023/11/News-6-Chaleya-Banner.jpg" },
+  { title: "Heeriye", artist: "Jasleen Royal", src: "https://res.cloudinary.com/doygacnfz/video/upload/v1774949962/Heeriye_z7vp1x.mp3", cover: "https://i.ytimg.com/vi/RLzC55ai0eo/maxresdefault.jpg" },
+  { title: "I Think They Call This Love", artist: "Unknown", src: "https://res.cloudinary.com/doygacnfz/video/upload/v1774949751/IThinkTheyCallThisLove_pd6ow1.mp3", cover: "https://axomlyrics.com/wp-content/uploads/2025/09/i-think-they-call-this-love-lyrics.webp" },
+  { title: "Kalyani", artist: "Unknown", src: "https://res.cloudinary.com/doygacnfz/video/upload/v1774950040/KALYANI_iglkfw.mp3", cover: "https://i.ytimg.com/vi/CqDByohxbtA/sddefault.jpg" },
+  { title: "Kesariya", artist: "Arijit Singh", src: "https://res.cloudinary.com/doygacnfz/video/upload/v1774950094/Kesariya_cxholq.mp3", cover: "https://www.easterneye.biz/wp-content/uploads/2024/05/kesariya-only-indian-song-to-surpass-500-million-streams-on-spotify.jpg" },
+  { title: "Lutt Putt Gaya", artist: "Arijit Singh", src: "https://res.cloudinary.com/doygacnfz/video/upload/v1774950091/LuttPuttGaya_x61tni.mp3", cover: "https://i.ytimg.com/vi/7CdpHATpXXU/maxresdefault.jpg" },
+  { title: "Pink Lips", artist: "Unknown", src: "https://res.cloudinary.com/doygacnfz/video/upload/v1774950181/PinkLips_momoql.mp3", cover: "https://i.ytimg.com/vi/w5VFOKKAbQQ/maxresdefault.jpg" },
+  { title: "Satranga", artist: "Arijit Singh", src: "https://res.cloudinary.com/doygacnfz/video/upload/v1774950194/Satranga_msnsp4.mp3", cover: "https://c.ndtvimg.com/2023-10/s8cqfkd8_-rashmika-_625x300_27_October_23.jpg" },
+  { title: "Tere Liye", artist: "Unknown", src: "https://res.cloudinary.com/doygacnfz/video/upload/v1774950217/TereLiye_ac1xxn.mp3", cover: "https://i.ytimg.com/vi/nOZ3M1O0Wbg/maxresdefault.jpg" },
+  { title: "Tu Hi Mera", artist: "Unknown", src: "https://res.cloudinary.com/doygacnfz/video/upload/v1774950204/TuHiMera_ovbhyy.mp3", cover: "https://i.ytimg.com/vi/yBa3FVQKAvY/maxresdefault.jpg" },
+  { title: "Tufan", artist: "Unknown", src: "https://res.cloudinary.com/doygacnfz/video/upload/v1774950198/Tufan_zh9dim.mp3", cover: "https://img.freepik.com/premium-photo/cute-piggy-simple-flat-clipart-cartoon-illustration-with-white-background-no-shadow-detail_1091925-12850.jpg?w=1380" },
+  { title: "Tera Deedar Hua", artist: "Unknown", src: "https://res.cloudinary.com/doygacnfz/video/upload/v1774950184/TeraDeedarHua_winzxc.mp3", cover: "https://i.ytimg.com/vi/abcd1234/maxresdefault.jpg" },
+  { title: "Kabhi Jo Badal Barse", artist: "Arijit Singh", src: "https://res.cloudinary.com/doygacnfz/video/upload/v1774949923/KabhiJoBadalBarse_fuytch.mp3", cover: "https://i.ytimg.com/vi/xyz123/maxresdefault.jpg" }
 ];
