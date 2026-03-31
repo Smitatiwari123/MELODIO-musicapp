b8f311469f0e840a0023b6d19900232a0eeeaccd
Smitatiwari123 <smita.tiwari.cs23@ggits.net>
2026-03-31 14:20:59 +0530

    Update tracks.js

--- tracks.js
+++ tracks.js
@@ -9,91 +9,91 @@ cover: "https://i.ytimg.com/vi/11AQjgYybZk/maxresdefault.jpg"
 {
 title: "Ban Ja Rani",
 artist: "Guru Randhawa",
-src: "BanJaRani.mp3",
+src: "songs/BanJaRani.mp3",
 cover: "https://i.ytimg.com/vi/hlvbDjksdCg/maxresdefault.jpg"
 },
 
 {
 title: "Chal Diye Tum Kahan",
 artist: "Unknown",
-src: "./songs/ChalDiyeTumKahan.mp3",
+src: "songs/ChalDiyeTumKahan.mp3",
 cover: "https://i.ytimg.com/vi/Qhl0bocazGQ/maxresdefault.jpg"
 },
 
 {
 title: "Chaleya",
 artist: "Arijit Singh",
-src: "chaleya.mp3",
+src: "songs/Chaleya.mp3",
 cover: "https://www.redchillies.com/wp-content/uploads/2023/11/News-6-Chaleya-Banner.jpg"
 },
 
 {
 title: "Heeriye",
 artist: "Jasleen Royal",
-src: "./songs/heeriye.mp3",
+src: "songs/Heeriye.mp3",
 cover: "https://i.ytimg.com/vi/RLzC55ai0eo/maxresdefault.jpg"
 },
 
 {
 title: "I Think They Call This Love",
 artist: "Unknown",
-src: "./songs/IThinkTheyCallThisLove.mp3",
+src: "songs/IThinkTheyCallThisLove.mp3",
 cover: "https://axomlyrics.com/wp-content/uploads/2025/09/i-think-they-call-this-love-lyrics.webp"
 },
 
 {
 title: "Kalyani",
 artist: "Unknown",
-src: "./songs/KALYANI.mp3",
+src: "songs/KALYANI.mp3",
 cover: "https://i.ytimg.com/vi/CqDByohxbtA/sddefault.jpg"
 },
 
 {
 title: "Kesariya",
 artist: "Arijit Singh",
-src: "./songs/Kesariya.mp3",
+src: "songs/Kesariya.mp3",
 cover: "https://www.easterneye.biz/wp-content/uploads/2024/05/kesariya-only-indian-song-to-surpass-500-million-streams-on-spotify.jpg"
 },
 
 {
 title: "Lutt Putt Gaya",
 artist: "Arijit Singh",
-src: "./songs/LuttPuttGaya.mp3",
+src: "songs/LuttPuttGaya.mp3",
 cover: "https://i.ytimg.com/vi/7CdpHATpXXU/maxresdefault.jpg"
 },
 
 {
 title: "Pink Lips",
 artist: "Unknown",
-src: "./songs/PinkLips.mp3",
+src: "songs/PinkLips.mp3",
 cover: "https://i.ytimg.com/vi/w5VFOKKAbQQ/maxresdefault.jpg"
 },
 
 {
 title: "Satranga",
 artist: "Arijit Singh",
-src: "./songs/Satranga.mp3",
+src: "songs/Satranga.mp3",
 cover: "https://c.ndtvimg.com/2023-10/s8cqfkd8_-rashmika-_625x300_27_October_23.jpg"
 },
 
 {
 title: "Tere Liye",
 artist: "Unknown",
-src: "./songs/TereLiye.mp3",
+src: "songs/TereLiye.mp3",
 cover: "https://i.ytimg.com/vi/nOZ3M1O0Wbg/maxresdefault.jpg"
 },
 
 {
 title: "Tu Hi Mera",
 artist: "Unknown",
-src: "./songs/TuHiMera.mp3",
+src: "songs/TuHiMera.mp3",
 cover: "https://i.ytimg.com/vi/yBa3FVQKAvY/maxresdefault.jpg"
 },
 
 {
 title: "Tufan",
 artist: "Unknown",
-src: "./songs/Tufan.mp3",
+src: "songs/Tufan.mp3",
 cover: "https://img.freepik.com/premium-photo/cute-piggy-simple-flat-clipart-cartoon-illustration-with-white-background-no-shadow-detail_1091925-12850.jpg?w=1380"
 }
 ];
